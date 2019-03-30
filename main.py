90c5537f7e860dc713e906c7ae51ec42929fc370
n00bmind <n00bmindr0b0t@gmail.com>
2019-03-30 19:17:19 +0000

    + Support completing paths for the second argument of '#add'

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import subprocess
 from pathlib import Path
 from prompt_toolkit import prompt, print_formatted_text, HTML, PromptSession
 from prompt_toolkit import print_formatted_text as printf
-from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.completion import Completer, WordCompleter, PathCompleter
 from prompt_toolkit.shortcuts import CompleteStyle
+from prompt_toolkit.document import Document
 
 
 commands_list = [
@@ -15,9 +16,50 @@ commands_list = [
 ]
 
 
+
+class CustomCompleter(Completer):
+    def __init__(self, completions, meta_dict, project_root):
+        self.words = WordCompleter(completions, meta_dict=meta_dict, WORD=True, ignore_case=True)
+        # TODO Rewrite using the one in prompt-toolkit/completion/filesystem.py as ref, but include '/' at the end of dir completions
+        self.paths = PathCompleter(get_paths=(lambda: [str(project_root)]))
+
+    def _split_args(self, text):
+        args = text.split()
+        cur_arg_index = len(args) - 1 if args and len(args) else 0
+
+        if text and text[-1] == ' ':
+            cur_arg_index += 1
+
+        return args, cur_arg_index
+
+    def get_completions(self, document, complete_event):
+        text = document.text_before_cursor
+        args, cur_arg_index = self._split_args(text)
+        cmd = args[0] if args else ''
+
+        is_directive = text and text[0] == '#'
+        needs_args = is_directive and cmd != '#quit'
+        if not needs_args and cur_arg_index > 0:
+            return
+
+        completer = self.words
+        # For now support paths only in second arg for #add
+        # FIXME Support spaces in paths!
+        is_path_arg = args and cmd == '#add' and cur_arg_index == 2
+        if is_path_arg:
+            completer = self.paths
+            # TODO Generalize for any argument position
+            cur_text = args[2] if len(args) == 3 else ''
+            cur_pos = len(args[2]) if len(args) == 3 else 0
+            document = Document(text=cur_text, cursor_position=cur_pos, selection=document.selection)
+
+        yield from completer.get_completions(document, complete_event)
+
+
+
 def parse_directive(text, project_path, entries_dict):
     running = True
-    reload_entries = False
+    entries_dirty = False
 
     args = text.split()
     cmd = args[0]
@@ -27,15 +69,15 @@ def parse_directive(text, project_path, entries_dict):
         if len(args) < 3:
             print('Syntax: #add [alias] [path]')
         else:
-            # FIXME Ensure relative path is given!
+            # TODO Ensure relative path is given!
             path = project_path / args[2] #os.path.join(project_path, args[2])
             if not path.exists(): #os.path.exists(path):
                 print(f'Invalid path {path}')
             else:
                 entries_dict[args[1]] = args[2]
-                reload_entries = True
+                entries_dirty = True
 
-    return running, reload_entries
+    return running, entries_dirty
 
 
 
@@ -54,17 +96,27 @@ def execute_entry(path):
 
 
 
+def build_completions_func(entries_dict):
+    def func():
+        return commands_list + list(entries_dict.keys())
+
+    return func
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('I need a path to the project\'s root')
         exit(1)
+
     project_path = Path(sys.argv[1])
-    project_file = project_path / 'project.plik'
-    print(repr(project_file))
     if not os.path.isdir(project_path):
         print('Invalid path')
         exit(1)
+
+    project_file = project_path / 'project.plik'
+    if os.path.isfile(project_file):
+        printf(HTML(f'<gray>Found project file at {project_file}</gray>'))
 
     entries_dict = {}
     try:
@@ -78,21 +130,19 @@ if __name__ == '__main__':
         exit(1)
 
 
-    completions = commands_list + list(entries_dict.keys())
-    completer = WordCompleter(completions, WORD=True)
+    completions_func = build_completions_func(entries_dict)
+    completer = CustomCompleter(completions_func, entries_dict, project_path)
     session = PromptSession()
 
     running = True
     while(running):
         # TODO Auto-complete paths in #add
-        # TODO Show path next to alias in auto-complete options?
-        text = session.prompt(HTML('<b><white>:: </white></b>'), vi_mode=True, completer=completer, complete_while_typing=False)
+        # TODO Key bindings?
+        text = session.prompt(HTML('<b><white>:: </white></b>'), vi_mode=False, completer=completer, complete_while_typing=False)
+                # complete_style=CompleteStyle.COLUMN)
         if text.startswith('#'):
-            running, reload_entries = parse_directive(text, project_path, entries_dict)
-            if reload_entries:
-                completions = commands_list + list(entries_dict.keys())
-                completer.words = completions
-
+            running, entries_dirty = parse_directive(text, project_path, entries_dict)
+            if entries_dirty:
                 with open(project_file, 'wt') as f:
                     json.dump(entries_dict, f, indent=4)
 
